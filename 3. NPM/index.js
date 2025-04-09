e00235e0d6e513e96ec9ccf88f7d2e98160530c4
Jason Wang <wanghung07@gmail.com>
2025-04-09 21:36:03 +0800

    add npm module lecture

---
+++ 3. NPM/index.js
@@ -0,0 +1,8 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+
+console.log(format(new Date(), "yyyy-MM-dd\tHH:mm:ss"));
+
+console.log("uuid: ", uuid());
+console.log("uuid: ", uuid());
+console.log("uuid: ", uuid());
